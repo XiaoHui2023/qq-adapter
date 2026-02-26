f3ac11a508aec0785b6f095e9f01315cc696374c
XiaoHui2023 <695001309@qq.com>
2026-02-26 17:06:18 +0800

    便捷启动脚本

--- update.bat
+++ update.bat
@@ -1,5 +1,12 @@
 @echo off
 cd /d %~dp0
+
+if not exist .venv (
+    echo Creating virtual environment...
+    python -m venv .venv
+)
+
+call .venv\Scripts\activate.bat
 git pull
 pip install -r requirements.txt
 pause
